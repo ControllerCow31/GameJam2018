9f1b737c1ee40f95b5730dd949b40b4fab09d2c2
Evanm <evan.mcclure31@gmail.com>
2018-11-03 10:43:05 -0700

    Rough integration of dash with movement

--- Assets/Prefabs/Player/PlayerBehavior.cs
+++ Assets/Prefabs/Player/PlayerBehavior.cs
@@ -6,20 +6,33 @@ public class PlayerBehavior : MonoBehaviour {
 	
 	public GameObject playerObject;
 	public Rigidbody2D playerRB;
+	public float speed;
 	public bool grounded;
+	public bool dashIsCharging;
+	public float dashTime;
+	public float startDashTime;
+	public float dashSpeed;
+	
 	// Use this for initialization
 	void Start () {
 		playerRB = playerObject.GetComponent<Rigidbody2D>();
 		grounded = false;
+		speed = 5f;
+		dashIsCharging = false;
+		dashSpeed = 50;
+		startDashTime = 0.2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float speed = 5f;
 		transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
 
 		if(Input.GetKeyDown(KeyCode.W) && grounded == true){
 			playerRB.AddForce(transform.up*1000);
+		}
+
+		if(Input.GetKey(KeyCode.Space)){
+			dash();
 		}
 	}
 	public void OnCollisionEnter2D(Collision2D other){
@@ -28,5 +41,41 @@ public class PlayerBehavior : MonoBehaviour {
 
 	public void OnCollisionExit2D(Collision2D other){
 		grounded = false;
+	}
+
+	public void dash(){
+		if (dashTime <= 0)
+        {
+            dashTime = startDashTime;
+            playerRB.velocity = Vector2.zero;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+
+            if (Input.GetButton("Horizontal") && Input.GetButtonDown("Jump"))
+            {
+                if (Input.GetAxis("Horizontal") > 0)
+                {
+                    playerRB.velocity += Vector2.right * dashSpeed;
+                }
+                if (Input.GetAxis("Horizontal") < 0)
+                {
+                    playerRB.velocity += Vector2.left * dashSpeed;
+                }
+            }
+
+            if (Input.GetButton("Vertical") && Input.GetButtonDown("Jump"))
+            {
+                if (Input.GetAxis("Vertical") > 0)
+                {
+                    playerRB.velocity += Vector2.up * dashSpeed;
+                }
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    playerRB.velocity += Vector2.down * dashSpeed;
+                }
+            }
+        }
 	}
 }
