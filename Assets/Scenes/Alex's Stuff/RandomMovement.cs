1dc3ba2aaccb69e14be5381687ea5fc01e4309d0
Alex Larsen <Yarf2000@sbcglobal.net>
2018-11-03 00:03:38 -0400

    FridayNight update
    got some random stuff for the baddies happening

---
+++ Assets/Scenes/Alex's Stuff/RandomMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour {
+    public GameObject enemy;
+    public float spawnTime = 1f;
+    public float betweenSpawn = 0f;
+
+
+    // Update is called once per frame
+    void Update () {
+        betweenSpawn += Time.deltaTime;
+
+        if (betweenSpawn >= spawnTime)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            betweenSpawn = 0;
+        }
+    }
+}
