3e8e45beb60b7eab0fd1a860d8fbdd46673f06c6
EricJeong05 <ericjhjeong@gmail.com>
2018-11-03 16:25:59 -0700

    Health bars

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public Slider bossHealthSlider;
+    public static float bossHealth = 100f;
+    public float damage = 5f;
+    public bool isDamaged;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (isDamaged) {
+            bossHealth -= damage;
+            bossHealthSlider.value = bossHealth;
+        }
+
+        isDamaged = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            isDamaged = true;
+        }
+    }
 }
