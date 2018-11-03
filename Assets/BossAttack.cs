6cb958684b1fc5a77b019653b26416b46cea1c48
EricJeong05 <ericjhjeong@gmail.com>
2018-11-03 14:29:45 -0700

    update

---
+++ Assets/BossAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public float bulletVelocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
