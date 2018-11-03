3789c69d1d830a981251e56e4caccf0d8910417a
Alex <yarf2000@sbcglobal.net>
2018-11-03 10:40:27 -0700

    Enemy
    implemented 2 enemies, one that just goes back and forth, and one that follows the player :)

---
+++ Assets/Scenes/Alex's Stuff/Patrol.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+	private float speed;
+	public float distance;
+
+	private bool movingRight = true;
+
+	public Transform groundDetection;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(2, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+		RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+		if (groundInfo.collider == false)
+		{
+			if (movingRight == true)
+			{
+				transform.eulerAngles = new Vector3(0, -180, 0);
+				movingRight = false;
+			}
+			else
+			{
+				transform.eulerAngles = new Vector3(0, 0, 0);
+				movingRight = true;
+			}
+
+		}
+		
+
+	}
+}
