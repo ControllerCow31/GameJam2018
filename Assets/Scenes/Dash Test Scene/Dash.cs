d4da9e48957792f85adcaeeeb3dbb0b1ada2132a
EricJeong05 <ericjhjeong@gmail.com>
2018-11-03 01:39:16 -0700

    Dash - feels better

--- Assets/Scenes/Dash Test Scene/Dash.cs
+++ Assets/Scenes/Dash Test Scene/Dash.cs
@@ -11,6 +11,7 @@ public class Dash : MonoBehaviour {
     float dashTime;
     float moveHorizontal;
     float moveVertical;
+    bool isGrounded;
 
     public float moveSpeed;
     public float dashSpeed;
@@ -22,59 +23,67 @@ public class Dash : MonoBehaviour {
         playerRigidBody = GetComponent<Rigidbody2D>();
         dashTime = startDashTime;
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 8) {
+            isGrounded = true;
+        }
     }
 
-    void FixedUpdate()
-    {
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.layer == 8) {
+            isGrounded = false;
+        }
+    }
+
+    // Called a set number of times per second
+    void FixedUpdate(){
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
-        playerRigidBody.velocity = new Vector2(moveHorizontal * moveSpeed, playerRigidBody.velocity.y);
+        //Only have control when
+        if (isGrounded) {
+            transform.Translate(moveHorizontal * moveSpeed * Time.deltaTime, 0, 0);
+        }
 
-        if (dashTime <= 0)
-        {
+        if (dashTime <= 0) {
             //Limit use of dashes with timer
             dashTime = startDashTime;
             playerRigidBody.velocity = Vector2.zero;
         }
-        else
-        {
+        else {
             dashTime -= Time.deltaTime;
             //Dash Left & Right
-            if (Input.GetButton("Horizontal") && Input.GetButtonDown("Jump"))
-            {
-                if (moveHorizontal > 0)
-                {
+            if (Input.GetButton("Horizontal") && Input.GetButtonDown("Jump")) {
+                if (moveHorizontal > 0) {
                     playerRigidBody.velocity += Vector2.right * dashSpeed;
                     StartCoroutine(cameraShake.Shake(dashTime, cameraShakeMagnitude));
                 }
-                if (moveHorizontal < 0)
-                {
+                if (moveHorizontal < 0) {
                     playerRigidBody.velocity += Vector2.left * dashSpeed;
                     StartCoroutine(cameraShake.Shake(dashTime, cameraShakeMagnitude));
                 }
             }
 
             //Dash Up & Down
-            if (Input.GetButton("Vertical") && Input.GetButtonDown("Jump"))
-            {
-                if (moveVertical > 0)
-                {
+            if (Input.GetButton("Vertical") && Input.GetButtonDown("Jump")) {
+                if (moveVertical > 0) {
                     playerRigidBody.velocity += Vector2.up * dashSpeed;
                     StartCoroutine(cameraShake.Shake(dashTime, cameraShakeMagnitude));
 
                 }
-                if (moveVertical < 0)
-                {
+                if (moveVertical < 0) {
                     playerRigidBody.velocity += Vector2.down * dashSpeed;
                     StartCoroutine(cameraShake.Shake(dashTime, cameraShakeMagnitude));
 
                 }
             }
+
+            /**if (Input.GetButton("Horizontal") && Input.GetButton("Vertical") && Input.GetButtonDown("Jump")) {
+                if (moveVertical > 0 && moveHorizontal > 0) {
+                    playerRigidBody.velocity += (Vector2.up + Vector2.right) * dashSpeed;
+                }
+            }*/
         }
     }
 }
