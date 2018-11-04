da0d47d91d686d90ea1627067eccd868350f62f3
EricJeong05 <ericjhjeong@gmail.com>
2018-11-03 21:52:49 -0700

    Added Knockback & Player Death script

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,7 +7,7 @@ public class PlayerHealth : MonoBehaviour {
 
 
     public Slider healthSlider;
-    public float playerHealth = 100f;
+    public float health = 100f;
     public float damage;
     public bool isDamaged;
     public float graceTime = 1f;
@@ -15,14 +15,14 @@ public class PlayerHealth : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        playerHealth = 100;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (isDamaged) {
-            playerHealth -= damage;
-            healthSlider.value = playerHealth;
+            health -= damage;
+            healthSlider.value = health;
         }
         else {
             healthSlider.value += .1f;
