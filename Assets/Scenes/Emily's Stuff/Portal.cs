fa794c605a35e80d8902d954966c79b17c8fdb2c
Evanm <evan.mcclure31@gmail.com>
2018-11-04 13:31:47 -0800

    fixed boss portal

--- Assets/Scenes/Emily's Stuff/Portal.cs
+++ Assets/Scenes/Emily's Stuff/Portal.cs
@@ -5,13 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour {
 
-    void OnTriggerEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("it worked"); 
-            SceneManager.LoadScene("Boss");
-            //Invoke("ReloadLevel", 1);  
+            Debug.Log("collided with portal");
+            SceneManager.LoadScene(2);
         }
     }
 }
