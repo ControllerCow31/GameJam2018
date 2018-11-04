ddb1a3d7b149675c677bd16c8f4bbd1fdea1e474
Alex Larsen <Yarf2000@sbcglobal.net>
2018-11-04 15:44:26 -0500

    Title Screen and other stuff

---
+++ Assets/Scenes/Alex's Stuff/TitleScreenScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleScreenScript : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Rough Level");
+    }
+}
