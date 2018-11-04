282a5d7ac1e2b1e28406003d874efb4f14b45663
ewarn99 <44716668+ewarn99@users.noreply.github.com>
2018-11-04 13:24:30 -0800

    Portal

---
+++ Assets/Scenes/Emily's Stuff/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("it worked"); 
+            SceneManager.LoadScene("Boss");
+            //Invoke("ReloadLevel", 1);  
+        }
+    }
+}
