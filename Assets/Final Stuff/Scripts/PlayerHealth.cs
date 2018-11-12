9b58f8e23b39277496c4f6b316832ff8689a35de
ewarn99 <44716668+ewarn99@users.noreply.github.com>
2018-11-12 14:09:55 -0800

    added i frames

--- Assets/Final Stuff/Scripts/PlayerHealth.cs
+++ Assets/Final Stuff/Scripts/PlayerHealth.cs
@@ -9,24 +9,29 @@ public class PlayerHealth : MonoBehaviour {
     public float health = 100f;
     public float damage;
     public bool isDamaged;
-    public float graceTime = 1f;
+    public float graceTime;
     float flashTimer = 0.25f;
+
+    private float damageTimer; 
 
     SpriteRenderer sprite;
 
     // Use this for initialization
     void Start () {
+        damageTimer = graceTime; 
         sprite = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (isDamaged) {
+        if (isDamaged && damageTimer <= 0) {
             health -= damage;
+            damageTimer = graceTime; 
             sprite.color = Color.red;
         }
         else {
             flashTimer -= Time.deltaTime;
+            damageTimer -= Time.deltaTime; 
             if (health < 100) {
                 health += 0.075f;
             }
